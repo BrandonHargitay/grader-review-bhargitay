be06b3954038b55c39e384520fb7560d5c9d92d2
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-11-28 10:17:14 -0800

    initial commit

--- grade.sh
+++ grade.sh
@@ -6,7 +6,36 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
+printf '\nFinished cloning\n\n'
+
+
+cd ./student-submission
+if [ -f "ListExamples.java" ]; then 
+  printf "ListExamples.java found!\n\n"
+else
+  printf "ListExamples.java not found, is your file named correctly?\n"
+  exit
+fi
+printf "Compiling files................\n\n"
+cd ../grading-area
+cp ../student-submission/ListExamples.java .
+cp ../TestListExamples.java .
+mkdir lib
+cp ../lib/*.jar ./lib
+touch file.txt
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java >> file.txt 2>&1
+exit_status=$?
+if [ $exit_status -eq 1 ]; then
+    printf "###########################################################\n"
+    printf "| ERROR! Your script did not complie due to the following |\n"
+    printf "###########################################################\n\n"
+    cat file.txt
+    exit
+else
+    printf "Files complied"g
+fi
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 
 # Draw a picture/take notes on the directory structure that's set up after
